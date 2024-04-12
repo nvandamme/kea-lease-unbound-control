a499425ffacc11262a038346260ebdd03123e04e
Nicolas Vandamme <n.vandamme@firis-sytem.lu>
2024-04-12 13:09:01 +0200

    better script help

--- kea-lease-unbound-control.sh
+++ kea-lease-unbound-control.sh
@@ -63,60 +63,38 @@ Setup:
 - Configure unbound-control to listen on 127.0.0.1:953
 - Configure unbound local_zone with a local domain, e.g.:
     - local-zone: "local." transparent
-- Configure kea to call this script with the appropriate hook: 
-    "hooks-libraries": [
-        {
-            "library": "/usr/local/lib/kea/hooks/libdhcp_run_script.so",
-            "parameters": {
-                "name": "/usr/local/bin/kea-lease-unbound-control.sh",
-                "sync": "false"
-            }
+- Configure kea to call this script with the appropriate hook:
+```json	
+"hooks-libraries": [
+    {
+        "library": "/usr/local/lib/kea/hooks/libdhcp_run_script.so",
+        "parameters": {
+            "name": "/path/to/kea-lease-unbound-control.sh",
+            "sync": "false"
         }
-    ]
-- pfSense users can use the pfSense package "Kea DHCP" to configure the hooks by modifiying /etc/inc/services.inc to activate kea's hooks and call this script
-    - in /etc/inc/services.inc, add the following lines to the `services_kea4_configure()` function:
-    ```php 
-        $kea_lease_cmds_hook = [
-                'library' => '/usr/local/lib/kea/hooks/libdhcp_lease_cmds.so',
-        ];
-
-        + $kea_run_script_hook = [
-        +         'library' => '/usr/local/lib/kea/hooks/libdhcp_run_script.so',
-        +         'parameters' => [
-        +                 'name' => '/root/kea-lease-unbound-control.sh',
-        +                 'sync' => false
-        +         ]
-        + ];
-        + 
-        + $keaconf['Dhcp4']['hooks-libraries'][] = $kea_run_script_hook;
-    ```
-    - in /etc/inc/services.inc, add the following lines to the `services_kea6_configure()` function:
-    ```php 
-        $kea_lease_cmds_hook = [
-                'library' => '/usr/local/lib/kea/hooks/libdhcp_lease_cmds.so',
-        ];
-
-        + $kea_run_script_hook = [
-        +         'library' => '/usr/local/lib/kea/hooks/libdhcp_run_script.so',
-        +         'parameters' => [
-        +                 'name' => '/root/kea-lease-unbound-control.sh',
-        +                 'sync' => false
-        +         ]
-        + ];
-        + 
-        + $keaconf['Dhcp6']['hooks-libraries'][] = $kea_run_script_hook;
-    ```
+    }
+]
+```
 - Configure environment variables via `/path/to/kea-lease-unbound-control.sh.env` or your preferred `etc` location:
+```sh
     UNBOUND_CONTROL_PATH="/usr/sbin/unbound-control"
     UNBOUND_CONFIG_PATH="/etc/unbound/unbound.conf"
     UNBOUND_CONTROL_IP="127.0.0.1"
     UNBOUND_CONTROL_PORT="953"
     LOG_ENABLED=1
     LOG_FILE="/var/log/kea-lease-unbound-control.log"
+```
+- Ensure the config is readable and the script is executable and readable by kea's process user:
+```sh
+chown kea:kea /path/to/kea-lease-unbound-control.sh
+chmod +x /path/to/kea-lease-unbound-control.sh
+chown kea:kea /path/to/kea-lease-unbound-control.sh.env
+```
 
 Options:
 -h, --help      Display this help and exit
 -v, --version   Output version information and exit
+-s, --setup     Display setup instructions
 EOF
 )
 
